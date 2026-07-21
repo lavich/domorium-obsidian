13d65e2c6a8f04256b8259b820492bc4b1530da6
Andrei Lobanov <lavich@magic4.pro>
2026-07-21 15:59:44 +0300

    chore: consume published language service

--- packages/validator/src/types/errors.ts
+++
@@ -1,9 +0,0 @@
-import { Range } from "./position";
-
-export interface GedcomError {
-  code: string;
-  message: string;
-  hint?: string;
-  range: Range;
-  level: "error" | "warning" | "info";
-}
